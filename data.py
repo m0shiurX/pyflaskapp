990b13c1333cbbffcf0f394592621452937d971b
Moshiur Rahman <unimrgm@gmail.com>
2017-08-21 14:37:56 +0600

    Flask app Basic Template Created

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'Author' : 'Moshour Rahman',
+            'body': 'lorem2 ipsum dolor silit',
+            'create date': '04-08-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'Author' : 'G Rahman',
+            'body': 'lorem2 ipsum dolor silit',            
+            'create date': '04-08-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article three',
+            'Author' : 'M Rahman',
+            'body': 'lorem2 ipsum dolor silit',            
+            'create date': '04-08-2017'
+        }
+    ]
+    return articles
